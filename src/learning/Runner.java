26c675e0447deb3c83fc27ad5101eab502299a8d
SirL0fty <sirl0fty@gmail.com>
2023-09-15 10:53:55 +0100

    initial commit

---
+++ src/learning/Runner.java
@@ -0,0 +1,23 @@
+package learning;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println( "Hello World ");
+
+        int number = 3;
+
+        System.out.println(number);
+
+        boolean yesNo = false;
+        byte snack = 0; // negative 128 127
+        char letter = 'A'; // character
+        short smallNumber = 15000; // small number
+        int regularNumber = 2000000000; // regular number
+        long bigNumber = 1234567891234567L; //L specifies a long number
+        float smallDecimal = 2.5f; // f specifies a float/decimal
+        double largeDecimal = 1.234567891234d; // d specifies a longer decimal
+
+        String message = "Hello this is message no. 32";
+
+    }
+}
