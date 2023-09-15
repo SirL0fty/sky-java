6b9c1b716e4969cc7f2dae25d57b14e8780b2806
SirL0fty <sirl0fty@gmail.com>
2023-09-15 12:17:45 +0100

    Created VariableSession

---
+++ src/learning/Variable.java
@@ -0,0 +1,32 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package learning;
+
+import java.util.Scanner;
+
+public class Variable {
+    public Variable() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your first name: ");
+        String nameFirstName = scanner.nextLine();
+
+        System.out.println("Please enter your last name: ");
+        String nameLastName = scanner.nextLine();
+
+        System.out.println("Hi " + nameFirstName + " " + nameLastName + ", Nice to meet you!");
+
+        System.out.println("What is your favourite number? ");
+        int favNum = scanner.nextInt();
+
+        System.out.println("What age are you? ");
+        int yourAge = scanner.nextInt();
+
+        System.out.println("Your favourite number and age added together is : " + (favNum + yourAge));
+    }
+}
