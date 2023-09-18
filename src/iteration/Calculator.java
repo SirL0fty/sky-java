efb2be624960a04529d4c425af74524c5e51b056
SirL0fty <sirl0fty@gmail.com>
2023-09-18 11:25:53 +0100

    Refactored Calc App

--- src/iteration/Calculator.java
+++ src/iteration/Calculator.java
@@ -7,36 +7,52 @@ public class Calculator {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please provide your first number? ");
-        int num1 = scanner.nextInt();
+        double num1 = scanner.nextDouble();
 
-        System.out.println("Enter the type of operation you want to perform (+, -, *, /): ");
-        String operator = scanner.next();
+        System.out.println("Enter the type of operation you want to perform (+, -, *, /, ^, %): ");
+        char operator = scanner.next().charAt(0);
 
         System.out.println("Please provide your second number? ");
-        int num2 = scanner.nextInt();
+        double num2 = scanner.nextDouble();
 
-        int total = 0;
+        double total = 0;
 
         switch (operator) {
-            case "+":
+            case '+':
                 total = num1 + num2;
-                System.out.println(total);
                 break;
-            case "-":
+            case '-':
                 total = num1 - num2;
-                System.out.println(total);
                 break;
-            case "/":
-                total = num1 / num2;
-                System.out.println(total);
+            case '/':
+                if (num2 != 0) {
+                    total = num1 / num2;
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                }
                 break;
-            case "*":
+            case '*':
                 total = num1 * num2;
-                System.out.println(total);
+                break;
+            case '%':
+                if (num2 != 0) {
+                    total = num1 % num2;
+                } else {
+                    System.out.println("Modulo by zero is not allowed.");
+                    return;
+                }
+                break;
+            case '^':
+                total = 1;
+                for (int i = 0; i < num2; i++) {
+                    total *= num1;
+                }
                 break;
             default:
-                System.out.println("Please enter a valid operator(+, -, *, /):" );
+                System.out.println("Please enter a valid operator (+, -, *, /, ^, %):");
+                return;
         }
 
+        System.out.println("Result: " + total);
     }
 }
---
+++ src/iteration/Calculator2.java
@@ -0,0 +1,12 @@
+package iteration;
+
+import java.util.Scanner;
+
+public class Calculator2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
